9ce526abb35fe8de68a9a6c9943d0b3212d65ebf
unknown <andreeamazere.am@gmail.com>
2025-11-16 02:50:55 +0200

    Add cbrn_perception package and update sim_env
    Introduced the cbrn_perception ROS 2 package with pose_estimator_node for human pose estimation using MediaPipe. Updated .gitignore for new package and Python artifacts. Modified sim_env launch file to reference cbrn_world.world and added placeholders for new nodes. Removed legacy human model and image, updated world to use person_standing, and adjusted package.xml and URDF for compatibility.

--- src/sim_env/launch/start_sim.launch.py
+++ src/sim_env/launch/start_sim.launch.py
@@ -1,51 +1,60 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch_ros.actions import Node 
 import xacro
 
 def generate_launch_description():
 
     pkg_dir = get_package_share_directory('sim_env')
-    world_file = os.path.join(pkg_dir, 'worlds', 'cbrn_test.world')
+    
+    rviz_config_file = os.path.join(pkg_dir, 'rviz', 'sim.rviz')
+    world_path = os.path.join(pkg_dir, 'worlds', 'cbrn_world.world')
     xacro_file = os.path.join(pkg_dir, 'urdf', 'cbrn_robot.urdf.xacro')
     robot_description_xml = xacro.process_file(xacro_file).toxml()
-    rviz_config_file = os.path.join(pkg_dir, 'rviz', 'sim.rviz')
 
     # =========== 1. Pornește Gazebo (Gz) HEADLESS ===========
-    # Încarcă lumea (care conține deja pacientul)
     gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+            )
+        ]),
         launch_arguments={
-            'gz_args': '-s -r ' + os.path.join('/home/andreea/cbrn_ws/src/sim_env/worlds', 'cbrn_test.world')
-        }.items(),
+            'gz_args': f'-v 4 -r --headless {world_path}'
+        }.items()
     )
 
-    # =========== 2. Pornește Robot State Publisher ===========
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description_xml, 'use_sim_time': True }]
-    )
+    #TO DO: Vezi ce e in neregula cu nodurile node_robot_state_publisher, spawn_entity
+    # Fara ele, gazebo simulation ruleaza, cu ele, se blocheaza
 
-    # =========== 3. Adaugă (Spawn) ROBOTUL în Lume ===========
-    # (NU pacientul)
-    spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-topic', 'robot_description',
-            '-entity', 'cbrn_robot',
-            '-x', '0', '-y', '0', '-z', '0.5'
-        ],
-        output='screen'
-    )
+    # # =========== 2. Pornește Robot State Publisher ===========
+    # node_robot_state_publisher = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[{'robot_description': robot_description_xml, 'use_sim_time': True }]
+    # )
 
-    # =========== 4. Pornește "Puntea" (Bridge) pentru Cameră (CORECTAT) ===========
+    # # =========== 3. Spawnează Robotul ===========
+    # spawn_entity = TimerAction(
+    #     period=3.0,  # așteaptă 3 secunde
+    #     actions=[Node(
+    #         package='ros_gz_sim',
+    #         executable='create',
+    #         arguments=[
+    #             '-world', 'cbrn_world',
+    #             '-topic', 'robot_description',
+    #             '-entity', 'cbrn_robot',
+    #             '-x', '0', '-y', '0', '-z', '0.5'
+    #         ],
+    #         output='screen'
+    #     )]
+    # )
+
+    # =========== 4. Pornește "Puntea" (Bridge) pentru Cameră ===========
     bridge_node = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -53,24 +62,34 @@ def generate_launch_description():
             # Folosim 'sensor_msgs/msg/Image' (sintaxa ROS 2 corectă)
             '/my_camera@sensor_msgs/msg/Image[gz.msgs.Image',
             '/my_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo'
+            '/my_camera@sensor_msgs/msg/Image[gz.msgs.Image]',
+            '/my_camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo]'
         ],
         output='screen'
     )
 
-    # =========== 5. Pornește RViz2 ===========
+   
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', rviz_config_file],
+        arguments=['-d', rviz_config_file], 
         output='screen'
     )
 
-    # Returnează lista de acțiuni de lansat
+    pose_estimator_node = Node(
+        package='cbrn_perception',        
+        executable='pose_estimator_node', 
+        name='pose_estimator_node',
+        output='screen'
+    )
+    
+
     return LaunchDescription([
         gz_sim,
         node_robot_state_publisher,
         spawn_entity,
-        bridge_node,
-        rviz_node,
+        # bridge_node,
+        # rviz_node,
+        # pose_estimator_node, # Adaugă nodul de percepție la lista de lansare
     ])
